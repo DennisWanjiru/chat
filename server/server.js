a212f518c0adab6f1801c9c2e38f3642e902484f
DennisWanjiru <denniswanjiru71@gmail.com>
2018-08-30 06:27:22 +0300

    Configure app ready for deployment

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 
+const PORT = process.env.PORT || 8000;
+
 const app = express();
 app.use(express.static(path.join(__dirname, '../client')));
 
-app.listen(8000, () => console.log('server runnig at port http://127.0.0.1:8000'));
+app.listen(PORT, () => console.log(`server runnig at port http://127.0.0.1:${PORT}`));
